74ae2f7993e7fa53c94bd32a650e68b91aa66a5b
Ederson <edersonbastiani@gmail.com>
2024-03-05 18:38:29 -0300

    Cors no controller

--- src/main/java/com/example/projeto/controllers/UserController.java
+++ src/main/java/com/example/projeto/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import com.example.projeto.service.UserService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
